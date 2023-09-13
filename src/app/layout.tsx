72d2cbb84e9c87be9cbe2d42f844e87b58ddafd1
Taohan Kang <46840416+takosenpai2687@users.noreply.github.com>
2023-09-13 15:10:59 +1000

    fix: font roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/components/navbar";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import "@fontsource/roboto";
 
 const roboto = Roboto({
     weight: ["100", "300", "400", "500", "700", "900"],
